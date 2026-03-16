ce1db4530bc16532f5cf1e5ad33c3abc79aecc9d
Manojxetri <khanalmanoj562@gmail.com>
2026-03-16 22:20:00 +0545

    changes

--- Components/Future/Future.jsx
+++ Components/Future/Future.jsx
@@ -4,26 +4,35 @@ import "./Future.css";
 const Future = () => {
   return (
     <div className="future">
+      {/* Watermark */}
       <div className="watermark">
         <h1 className="media-watermark">MEDIA TEAM</h1>
       </div>
 
-      <div className="future-images">
-        <img src="/images/drone.png" className="img1" alt="drone" />
-        <img src="/images/camera.png" className="img2" alt="camera" />
-      </div>
+      {/* Main content */}
+      <div className="future-content">
+        {/* Left Image */}
+        <div className="future-image left">
+          <img src="/images/drone.png" alt="drone" />
+        </div>
 
-      <div className="future-heading">
-        <h1>
-          Growing Together <span>for a Better Future</span>
-        </h1>
-      </div>
+        {/* Center Text */}
+        <div className="future-text">
+          <h1>
+            Growing Together <span>for a Better Future</span>
+          </h1>
+          <p>
+            At Nepathya Media Team, we believe in growing together through
+            creativity, collaboration, and innovation. By sharing ideas and
+            supporting one another, we aim to build a stronger community and
+            create meaningful content that inspires a better future for everyone.
+          </p>
+        </div>
 
-      <div className="future-para">
-        At Nepathya Media Team, we believe in growing together through
-        creativity, collaboration, and innovation. By sharing ideas and
-        supporting one another, we aim to build a stronger community and create
-        meaningful content that inspires a better future for everyone.
+        {/* Right Image */}
+        <div className="future-image right">
+          <img src="/images/camera.png" alt="camera" />
+        </div>
       </div>
     </div>
   );
